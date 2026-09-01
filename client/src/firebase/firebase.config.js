6063ddac6f4bb9d4a40083e59a5c580f90a822bc
Farhan Habib <farhan.habib.7552@gmail.com>
2026-09-01 18:28:52 +0530

    client URL added

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -5,9 +5,9 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId:
-    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiUrl: import.meta.env.VITE_API_URL,
 };
 
 const app = initializeApp(firebaseConfig);
